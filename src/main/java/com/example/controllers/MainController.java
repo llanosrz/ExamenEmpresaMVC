9e3665f5021e683bf9ad7075b265ee48bc67dc60
llruiz <llanosrzm@gmail.com>
2023-03-24 13:25:05 +0100

    Listar Empleados

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,5 +1,52 @@
 package com.example.controllers;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.entities.Empleado;
+import com.example.services.CorreoService;
+import com.example.services.DepartamentoService;
+import com.example.services.EmpleadoService;
+import com.example.services.TelefonoService;
+
+@Controller
+@RequestMapping("/")
 public class MainController {
     
+    private static final Logger LOG = Logger.getLogger("MainController");
+
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    @Autowired
+    private TelefonoService telefonoService;
+
+    @Autowired
+    private CorreoService correoService;
+
+    //Listar empleados
+
+    @GetMapping("/listar")
+    public ModelAndView listar() {
+
+        List<Empleado> empleados = empleadoService.findAll();
+        
+        ModelAndView mav = new ModelAndView("views/listarEmpleados");
+        mav.addObject("empleados", empleados);
+
+        return mav;
+    }
+
+
+
 }
