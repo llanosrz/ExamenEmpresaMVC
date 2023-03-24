81fc883bb5bd0a8d257269f311daec3ca40a6b93
llruiz <llanosrzm@gmail.com>
2023-03-24 08:54:29 +0100

    Creacion de entidades

---
+++ src/main/java/com/example/entities/Correo.java
@@ -0,0 +1,34 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "correos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Correo implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "El email no puede ser null")
+    private String email;
+    
+}
+
+
---
+++ src/main/java/com/example/entities/Departamento.java
@@ -0,0 +1,35 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name="departamentos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Departamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Size(max = 25, min = 4)
+    private String nombre;
+
+    
+}
